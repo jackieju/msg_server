afc8a68192da915894e41694e3c49e1a8f196334
jackie <jackie.ju@gmail.com>
2017-10-24 17:50:06 +0800

    migrate to aliyun

--- lib/server_settings.rb
+++ lib/server_settings.rb
@@ -3,16 +3,35 @@ def g_valid_msg_client_ip
 end
 
 def g_memcached_port
-    "11211"
+    "11231"
 end
 
 def g_launchables
    # ["battled", "global_quest", "rank", "daily", "hourly", "5min", "refresh", "sec", "sec2", "calloutd"]
-   ["cron_gserver"]
+   #["cron_gserver"]
+   ["daily"]
    #["battled", "global_quest", "rank", "daily", "hourly", "5min", "refresh", "sec", "sec2", "statisticsd"]
    
 end
 
 def g_FILEROOT
     "/var/wh"
+end
+
+def g_memcacheds
+    {
+        "11231"=>{
+            #:size=>256
+            :size=>32
+        }
+    }
+end
+
+def g_clusters
+    ports1 = 7620..7629
+    #ports2 = 6832..6834
+    #ports3 = 7025..7025
+    
+    clusters=[ports1]
+    return clusters
 end
